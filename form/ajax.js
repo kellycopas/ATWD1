c03a47617e4c85a4c37562a8a1ad8691ea3661f9
kellycopas <39497444+kellycopas@users.noreply.github.com>
2018-11-11 13:35:43 +0000

    Add files via upload

---
+++ form/ajax.js
@@ -0,0 +1,55 @@
+/*function findData(str) {
+	if (str.length==0) {
+		document.getElementById("data").innerHTML="";
+		return;
+	}
+	if (window.XMLHttpRequest) {
+		xmlhttp = new XMLHttpRequest();
+	} else {
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("data").innerHTML = this.responseText;
+	}
+}
+
+xmlhttp.open("GET","currPost.php?q="+str,true);
+xmlhttp.send();
+}*/
+
+/*function loadCurrPost() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("container").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "currPost.php", true);
+	xhttp.send();
+}*/
+
+function loadCurrPut() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("put_response").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "currPut.php", true);
+	xhttp.send();
+}
+
+function loadCurrDel() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("delete_response").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "currDelete.php?code=", true);
+	xhttp.send();
+}
